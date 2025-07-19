d78b7d716914f1dae9a78a3ec7cf9f087971b7a5
cristian urrutia <urrutiacristian86@gmail.com>
2025-07-18 21:37:40 -0500

    primer commit

--- inicioDeseccion.html/pantalla6.js
+++ inicioDeseccion.html/pantalla6.js
@@ -1,30 +1,5 @@
-
-  document.addEventListener('DOMContentLoaded', () => {
-    const botonMenu = document.querySelector('.abrirMenu');
-    const menu = document.querySelector('.menu');
-
-    botonMenu.addEventListener('click', () => {
-     
-      if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-      } else {
-        menu.style.display = 'flex';
-      }
-    });
-  });
-
-  document.addEventListener('DOMContentLoaded', () => {
-  const botonMenu = document.querySelector('.abrirMenu');
-  const menu = document.querySelector('.menu');
-  const botonCerrar = document.querySelector('.cerrarMenu');
-
-  botonMenu.addEventListener('click', () => {
-    menu.style.display = 'flex';
-  });
-
-  botonCerrar.addEventListener('click', () => {
-    menu.style.display = 'none';
-  });
-});
+ function toggleMenu() {
+      document.getElementById('menuLateral').classList.toggle('oculto');
+    }
 
   
